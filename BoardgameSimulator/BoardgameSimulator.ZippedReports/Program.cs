5655059294a069bc5ff6c315947dd2a14f833c69
SimoPrG <simeon_petev@hotmail.com>
2015-10-08 04:03:49 +0300

    Created generator for the zipped reports

---
+++ BoardgameSimulator/BoardgameSimulator.ZippedReports/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace BoardgameSimulator.ZippedReports
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var rnd = new Random();
+            var generator = new XlsReportGenerator();
+
+            const string RootFolder = @"C:\Temp\DatabasesTeamworkBattlesReport";
+            var workingDirectory = Path.Combine(RootFolder, "Working");
+
+            var greatestWarStartDate = new DateTime(121, 3, 31);
+            var currentDate = greatestWarStartDate;
+
+            int daysWithBattlesCount = rnd.Next(100, 301);
+            for (int i = 0; i < daysWithBattlesCount; i++)
+            {
+                currentDate = currentDate.AddDays(1 + rnd.NextDouble() * 3);
+                string parsedDate = currentDate.ToString("dd-MMM-yyyy");
+                string currentFolder = Path.Combine(workingDirectory, parsedDate);
+                Directory.CreateDirectory(currentFolder);
+
+                int battlesCount = rnd.Next(20, 201);
+                for (int j = 1; j <= battlesCount; j++)
+                {
+                    generator.GenerateXls(currentFolder + "\\Battle-" + j);
+                }
+            }
+
+            ZipFile.CreateFromDirectory(workingDirectory, RootFolder + "\\Battles.zip");
+
+            Directory.Delete(workingDirectory, true);
+
+            Console.WriteLine("Generating of battle reports completed!");
+        }
+    }
+}
---
+++ BoardgameSimulator/BoardgameSimulator.ZippedReports/XlsReportGenerator.cs
@@ -0,0 +1,66 @@
+﻿namespace BoardgameSimulator.ZippedReports
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Reflection;
+    using System.Runtime.InteropServices;
+
+    using Excel = Microsoft.Office.Interop.Excel;
+
+    public class XlsReportGenerator
+    {
+        private readonly Random rnd = new Random();
+
+        private readonly List<string> alignments = new List<string>() { "Evil", "Good", "Neutral", "Demigod", "Mythical" };
+        private readonly List<string> heroes = new List<string>() { "Bron", "Tor", "Batman", "Dr. Doom", "Grodd", "Hellboy", "Gordon", "Bizzaro", "Sabertooth", "Maxx" };
+        private readonly List<string> units = new List<string>() { "Elven Archer", "Shieldmaiden", "Pegasus", "Pixie", "Swordsman" };
+
+        public void GenerateXls(string fileName)
+        {
+            var excelApp = new Excel.Application { Visible = false };
+            var excelWorkbooks = excelApp.Workbooks;
+            var excelWorkbook = excelWorkbooks.Add(Missing.Value);
+            Excel.Worksheet excelWorksheet = excelWorkbook.ActiveSheet;
+
+            // Populate first row
+            excelWorksheet.Cells[1, 2] = "Alignment";
+            excelWorksheet.Cells[1, 3] = "Hero";
+            excelWorksheet.Cells[1, 4] = "Unit";
+            excelWorksheet.Cells[1, 5] = "Unit Quantity";
+
+            excelWorksheet.Cells[1, 7] = "Duration (seconds)";
+
+            // Populate first column
+            excelWorksheet.Cells[2, 1] = "First Army";
+            excelWorksheet.Cells[3, 1] = "Second Army";
+
+            // Populate alignments
+            excelWorksheet.Cells[2, 2] = this.alignments[this.rnd.Next(this.alignments.Count)];
+            excelWorksheet.Cells[3, 2] = this.alignments[this.rnd.Next(this.alignments.Count)];
+
+            // Populate heroes
+            excelWorksheet.Cells[2, 3] = this.heroes[this.rnd.Next(this.heroes.Count)];
+            excelWorksheet.Cells[3, 3] = this.heroes[this.rnd.Next(this.heroes.Count)];
+
+            // Populate units
+            excelWorksheet.Cells[2, 4] = this.units[this.rnd.Next(this.units.Count)];
+            excelWorksheet.Cells[3, 4] = this.units[this.rnd.Next(this.units.Count)];
+
+            // Populate unit quantities
+            excelWorksheet.Cells[2, 5] = this.rnd.Next(1, 201) * 5;
+            excelWorksheet.Cells[3, 5] = this.rnd.Next(1, 201) * 5;
+
+            // Duration
+            excelWorksheet.Cells[2, 7] = this.rnd.Next(10, 1001);
+
+            // Save and Release COM objects
+            Marshal.ReleaseComObject(excelWorksheet);
+            excelWorkbook.SaveAs(fileName);
+            excelWorkbook.Close(true);
+            Marshal.ReleaseComObject(excelWorkbook);
+            Marshal.ReleaseComObject(excelWorkbooks);
+            excelApp.Quit();
+            Marshal.ReleaseComObject(excelApp);
+        }
+    }
+}
