09676810c36899bc2ab489d253f1dca929bf839b
SimoPrG <simeon_petev@hotmail.com>
2015-10-16 19:12:36 +0300

    Optimized XlsReportGenerator

--- BoardgameSimulator/BoardgameSimulator.ZippedReports/XlsReportGenerator.cs
+++ BoardgameSimulator/BoardgameSimulator.ZippedReports/XlsReportGenerator.cs
@@ -4,23 +4,23 @@
     using System.IO;
     using System.Reflection;
     using System.Runtime.InteropServices;
+
     using Microsoft.Office.Interop.Excel;
 
     public class XlsReportGenerator : IDisposable
     {
-        private const string HeroId = "HeroID";
-        private const string UnitsId = "UnitsID";
-        private const string UnitsQuantity = "Units Quantity";
-
         private const string AlignmentFolderName = "AlignmentID-";
         private const string ReportFileName = "Report-";
 
-        private readonly Application app;
-        private readonly Workbooks workbooks;
-        private readonly Workbook workbook;
-        private readonly Worksheet worksheet;
+        private const int StartRow = 2;
+        private static readonly string[] FirstRowInSheet = { "HeroID", "UnitsID", "Units Quantity" };
 
         private readonly Random rnd;
+
+        private readonly Application app;
+        private readonly Workbook workbook;
+        private readonly Workbooks workbooks;
+        private readonly Worksheet worksheet;
 
         public XlsReportGenerator()
         {
@@ -51,25 +51,37 @@
 
         /// <summary>
         /// Generates Excel reports for the armies of the alignments.
-        /// Each alignment has a separate folder named AlignmentID-X where X is the alignment id.
-        /// In each folder there are random number of reports.
-        /// Each report has a random number of lines giving information for each army of the alignment.
+        ///     Each alignment has a separate folder named AlignmentID-X where X is the alignment id.
+        ///     In each folder there are random number of reports.
+        ///     Each report has a random number of lines giving information for each army of the alignment.
         /// </summary>
-        /// <param name="alignmentCount">The count of alignments</param>
-        /// <param name="minReportsCountPerAlignment">The minimum number of filereports you want per alignment</param>
-        /// <param name="maxReportsCountPerAlignment">The maximum number of filereports you want per alignment</param>
-        /// <param name="minArmiesPerAlignmentCount">The minimum number of lines (armies) in a single filereport</param>
-        /// <param name="maxArmiesPerAlignmentCount">The maximum number of lines (armies) in a single filereport</param>
-        /// <param name="rootDirectory">The directory into which all AlignmentID-X directories will be created.</param>
+        /// <param name="alignmentCount">
+        /// The count of alignments
+        /// </param>
+        /// <param name="minReportsCountPerAlignment">
+        /// The minimum number of filereports you want per alignment
+        /// </param>
+        /// <param name="maxReportsCountPerAlignment">
+        /// The maximum number of filereports you want per alignment
+        /// </param>
+        /// <param name="minArmiesPerAlignmentCount">
+        /// The minimum number of lines (armies) in a single filereport
+        /// </param>
+        /// <param name="maxArmiesPerAlignmentCount">
+        /// The maximum number of lines (armies) in a single filereport
+        /// </param>
+        /// <param name="rootDirectory">
+        /// The directory into which all AlignmentID-X directories will be created.
+        /// </param>
         public void GenerateXlsAlignmentsReports(
-            int alignmentCount,
-            int minReportsCountPerAlignment,
-            int maxReportsCountPerAlignment,
-            int minArmiesPerAlignmentCount,
-            int maxArmiesPerAlignmentCount,
+            int alignmentCount, 
+            int minReportsCountPerAlignment, 
+            int maxReportsCountPerAlignment, 
+            int minArmiesPerAlignmentCount, 
+            int maxArmiesPerAlignmentCount, 
             string rootDirectory)
         {
-            this.FillFirtsRowWithTitles(HeroId, UnitsId, UnitsQuantity);
+            this.FillRow(1, FirstRowInSheet);
 
             for (int currentAlignment = 1; currentAlignment <= alignmentCount; currentAlignment++)
             {
@@ -81,51 +93,41 @@
             }
         }
 
-        private void FillFirtsRowWithTitles(params string[] titles)
-        {
-            for (int i = 0; i < titles.Length; i++)
-            {
-                this.worksheet.Cells[1, i + 1] = titles[i];
-            }
-        }
-
-        private void SaveReports(int reportsCount, string currentFolder, int minArmiesPerAlignmentCount, int maxArmiesPerAlignmentCount)
+        private void SaveReports(
+            int reportsCount, 
+            string currentFolder, 
+            int minArmiesPerAlignmentCount, 
+            int maxArmiesPerAlignmentCount)
         {
             for (int currentReport = 1; currentReport <= reportsCount; currentReport++)
             {
                 int rowsCount = this.rnd.Next(minArmiesPerAlignmentCount, maxArmiesPerAlignmentCount + 1);
 
-                this.WriteDataInWorksheet(2, rowsCount);
+                for (int currentRowNumber = StartRow; currentRowNumber < StartRow + rowsCount; currentRowNumber++)
+                {
+                    // TODO: Get this hardcoded values from some Constants class when available.
+                    string heroId = this.rnd.Next(1, 201).ToString();
+                    string unitsId = this.rnd.Next(1, 201).ToString();
+                    string unitsQuantity = (this.rnd.Next(1, 1001) * 10).ToString();
+
+                    this.FillRow(currentRowNumber, heroId, unitsId, unitsQuantity);
+                }
 
                 this.workbook.SaveAs(Path.Combine(currentFolder, ReportFileName + currentReport));
 
-                this.CleanUpDataFromWorksheet(2, rowsCount);
+                // Clean up the worksheet for the next iteration.
+                for (int currentRowNumber = StartRow; currentRowNumber < StartRow + rowsCount; currentRowNumber++)
+                {
+                    this.FillRow(currentRowNumber, null, null, null);
+                }
             }
         }
 
-        private void WriteDataInWorksheet(int startingRow, int rowsCount)
+        private void FillRow(int row, params string[] cells)
         {
-            for (int currentRow = startingRow; currentRow < startingRow + rowsCount; currentRow++)
+            for (int i = 0; i < cells.Length; i++)
             {
-                // TODO: Get this hardcoded values from some Constants class when available.
-                // Write Hero ID
-                this.worksheet.Cells[currentRow, 1] = this.rnd.Next(1, 201);
-
-                // Write Units ID
-                this.worksheet.Cells[currentRow, 2] = this.rnd.Next(1, 201);
-
-                // Write Units Quantity
-                this.worksheet.Cells[currentRow, 3] = this.rnd.Next(1, 1001) * 10;
-            }
-        }
-
-        private void CleanUpDataFromWorksheet(int startingRow, int rowsCount)
-        {
-            for (int currentRow = startingRow; currentRow < startingRow + rowsCount; currentRow++)
-            {
-                this.worksheet.Cells[currentRow, 1] = null;
-                this.worksheet.Cells[currentRow, 2] = null;
-                this.worksheet.Cells[currentRow, 3] = null;
+                this.worksheet.Cells[row, i + 1] = cells[i];
             }
         }
     }
