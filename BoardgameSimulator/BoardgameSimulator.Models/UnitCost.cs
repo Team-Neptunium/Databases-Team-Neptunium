96ef2564107c9cd6d39d7b4011af210e5862002c
Pip3r4o <pip3r4o@gmail.com>
2015-10-17 00:32:31 +0300

    sqlite unitscost interface is now accessible

--- BoardgameSimulator/BoardgameSimulator.Models/UnitCost.cs
+++ BoardgameSimulator/BoardgameSimulator.Models/UnitCost.cs
@@ -1,9 +1,14 @@
 ﻿namespace BoardgameSimulator.Models
 {
-    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
+    [Table("UnitCost")]
     public class UnitCost
     {
+        [Key]
+        public int Id { get; set; }
+
         public string UnitName { get; set; }
 
         public int RecruitCost { get; set; }
---
+++ BoardgameSimulator/BoardgameSimulator.SQLiteDB/SqLiteDataSeeder.cs
@@ -0,0 +1,41 @@
+﻿namespace BoardgameSimulator.SQLiteDB
+{
+    using System;
+    using System.Linq;
+    using Data;
+    using Models;
+
+    public class SqLiteDataSeeder
+    {
+        public static void Seed(int entries = 100)
+        {
+            var data = new BoardgameSimulatorSqLiteData();
+
+            var oldEntries = data.UnitsCosts.All();
+
+            foreach (var entry in oldEntries)
+            {
+                data.UnitsCosts.Delete(entry);
+            }
+
+            var random = new Random();
+
+            var sqldata = new BoardgameSimulatorData();
+
+            var unitNames = sqldata.Units.All().Select(u => u.Name).ToList();
+
+            foreach (var unit in unitNames)
+            {
+                var randomNumber = random.Next(150, 3500);
+
+                data.UnitsCosts.Add(new UnitCost
+                {
+                    UnitName = unit,
+                    RecruitCost = randomNumber
+                });
+            }
+
+            data.UnitsCosts.SaveChanges();
+        }
+    }
+}
