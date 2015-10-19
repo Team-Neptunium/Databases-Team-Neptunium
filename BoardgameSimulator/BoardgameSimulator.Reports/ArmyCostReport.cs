6d3d0ddc0df5037a43aaa362bcacaa5f80ff89b3
Pip3r4o <pip3r4o@gmail.com>
2015-10-19 15:14:12 +0300

    Excel generator now fully functional - generates an army battle log report

---
+++ BoardgameSimulator/BoardgameSimulator.Reports/ArmyCostReport.cs
@@ -0,0 +1,25 @@
+﻿namespace BoardgameSimulator.Reports
+{
+    using System;
+
+    public class ArmyCostReport
+    {
+        public int Army1Id { get; set; }
+
+        public string UnitName1 { get; set; }
+
+        public int UnitQuantity1 { get; set; }
+
+        public int UnitCost1 { get; set; }
+
+        public int Army2Id { get; set; }
+
+        public string UnitName2 { get; set; }
+
+        public int UnitQuantity2 { get; set; }
+
+        public int UnitCost2 { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
